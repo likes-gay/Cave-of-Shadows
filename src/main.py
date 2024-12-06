b4e3289001cb6e9ceddb216b1d301a2aac153cfc
Yummy_Bacon5 <68166338+YummyBacon5@users.noreply.github.com>
2024-12-06 13:15:20 +0000

    Update exit method

--- src/main.py
+++ src/main.py
@@ -364,7 +364,7 @@ if __name__ == "__main__":
 		elif choice == 3:
 			print(f"{Fore.GREEN}Thanks for playing! Goodbye.{Style.RESET_ALL}")
 			deinit()
-			exit(1)
+			break
 
 		while player.play_game():
 			pass
